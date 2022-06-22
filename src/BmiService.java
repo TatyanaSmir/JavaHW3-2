3e6fdb70ae05135e5175f5d857b61eb613c9eff7
TatyanaSmir <tatyanasmirnova404@gmail.com>
2022-06-22 15:02:02 +0300

    ИМТ

---
+++ src/BmiService.java
@@ -0,0 +1,13 @@
+public class BmiService {
+
+
+    public int calculate ( int weight, double height) {
+
+        double height2 = height * height;
+        double imt = weight / height2;
+
+        return (int) Math.round(imt);
+
+    }
+
+}
