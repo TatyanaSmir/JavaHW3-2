7695967fbde4de9441193907f7e8527bab22e2e4
TatyanaSmir <tatyanasmirnova404@gmail.com>
2022-06-23 15:44:59 +0300

    Почистила код

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,12 +1,9 @@
 public class BmiService {
-
-
+    
     public double calculate(double weight, double height) {
 
         double height2 = height * height;
-        double imt = weight / height2;
-
-        return imt;
+        return weight / height2;
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double weight = 80;// в килограммах
-        double height = 1.75; // в метрах
-        double imt = service.calculate(weight, height);
+
+        double imt = service.calculate(80, 1.75);
         String result = String.format("%.2f", imt);
         System.out.print("ИМТ = " + result);
 
